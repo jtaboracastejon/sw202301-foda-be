f8dd925b8d2d540f01799751851a5c23e29b8972
jtaboracastejon <javiertaboracastejon@gmail.com>
2023-02-06 17:21:27 -0600

    Routes path for Empresas, Usuarios

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 const router  = express.Router();
 
+import empresasRouter from './empresas/empresas';
+import usuariosRouter from './usuarios/usuarios';
+
 router.get('/', (_req, res) => {
   res.json({msg:'Hello World!'});
- });
+});
+
+router.use('/empresas', empresasRouter);
+router.use('/usuarios', usuariosRouter);
 
 export default router;
