3387699d03e7179cd2e46f0d80065591c66497ac
jtaboracastejon <javiertaboracastejon@gmail.com>
2023-02-28 17:52:41 -0600

    Foda Dao

---
+++ src/dao/models/FODA/IFoda.ts
@@ -0,0 +1,45 @@
+import { ObjectId } from "mongodb";
+import { IAuditable } from "../IAuditable";
+export interface IFoda extends IAuditable{
+    _id?: ObjectId | String;
+    nombre: string,
+    owner: {
+        id: ObjectId,
+        usuario: string,
+        email: string
+    },
+    empresa:{
+        id: ObjectId,
+        nombre: string
+    },
+    entradas: number,
+    observacion?: string,
+    Fcantidad: number,
+    Ocantidad: number,
+    Dcantidad: number,
+    Acantidad: number,
+}
+
+export const DefaultFoda: IFoda = {
+    nombre: "",
+    owner: {
+        id: new ObjectId,
+        usuario: "",
+        email: ""
+    },
+    empresa: {
+        id: new ObjectId,
+        nombre: ""
+    },
+    entradas: 0,
+    Fcantidad: 0,
+    Ocantidad: 0,
+    Dcantidad: 0,
+    Acantidad: 0,
+    createdAt: new Date(),
+    updatedAt: new Date()
+}
+
+export interface IFodaEntry{
+
+}
